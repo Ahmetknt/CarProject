26da670daaec7f5e03a48bfe04dd61202b33829d
Ahmetknt <ahmtknt46@gmail.com>
2022-10-02 21:03:05 +0300

    update

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -10,16 +10,40 @@ namespace Business.Concrete
 {
     public class CarManager : ICarService
     {
-        ICarDal _iCarDal;
+        ICarDal _carDal;
 
         public CarManager(ICarDal iCarDal)
         {
-            _iCarDal = iCarDal;
+            _carDal = iCarDal;
+        }
+
+        public void Add(Car car)
+        {
+            if (car.Description.Length > 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Açıklama 2 karakterden fazla ve günlük fiyatı 0 dan büyük olmalı.");
+            }
+
+            
         }
 
         public List<Car> GetAll()
         {
-            return _iCarDal.GetAll();
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c=>c.BrandId==brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,11 +10,40 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
 
+
+            GetAllBrands(brandManager);
+            Console.WriteLine("-------------------------------");
+            GetAllCars(carManager);
+            Console.WriteLine("-------------------------------");
+            GetAllColors(colorManager);
+            
+        }
+
+        private static void GetAllBrands(BrandManager brandManager)
+        {
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+        }
+
+        private static void GetAllCars(CarManager carManager)
+        {
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
+            }
+        }
+
+        private static void GetAllColors(ColorManager colorManager)
+        {
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
             }
         }
     }
