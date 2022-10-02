33433d88b8b880f8bc6c1b8e4c34c7c4dde0fb19
Ahmetknt <ahmtknt46@gmail.com>
2022-10-02 17:31:35 +0300

    update

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -5,13 +5,13 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Car : IEntiy
+    public class Car : IEntity
     {
         public int Id { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
-        public short ModelYear { get; set; }
-        public short DailyPrice { get; set; }
+        public int ModelYear { get; set; }
+        public int DailyPrice { get; set; }
         public string Description { get; set; }
     }
 }
